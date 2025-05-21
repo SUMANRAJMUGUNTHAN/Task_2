8d9427943967610b768d4b076afdd09fa43355c9
SUMANRAJMUGUNTHAN <2122k0807sumanraj.m@gmail.com>
2025-05-21 10:28:30 +0530

    I write a simple addition function

---
+++ Sample.java
@@ -0,0 +1,9 @@
+public class Sample{
+    public static void main(String[] args) {
+        
+        int a =5;
+        int b =5;
+
+        System.out.println("The sum of number is" +a+b);
+    }
+}
